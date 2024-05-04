7746367fbe485ea493aafe98643a3abf3ce9086d
Johannes Ackermann <johannes.ackermann@looneylizard.com>
2024-05-04 14:41:21 +0200

    player sprites

--- player.gd
+++ player.gd
@@ -1,8 +1,23 @@
 extends CharacterBody2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 @export var player_number : int = 1
 
 const SPEED = 600.0
+
+func _ready() -> void:
+	var selection : StartRound.Selection = StartRound.Selection.NONE
+	match player_number:
+		1: selection = GameManager.player1_selection
+		2: selection = GameManager.player2_selection
+	match selection:
+		StartRound.Selection.ROCK:
+			sprite_2d.texture = load(str("res://assets/Hands/", player_number, "R.png"))
+		StartRound.Selection.PAPER:
+			sprite_2d.texture = load(str("res://assets/Hands/", player_number, "P.png"))
+		StartRound.Selection.SCISSORS:
+			sprite_2d.texture = load(str("res://assets/Hands/", player_number, "S.png"))
 
 func _physics_process(delta: float) -> void:
 	
